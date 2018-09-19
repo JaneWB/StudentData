54a2dd77cd7ecf108cdde671404d59a13c206f9e
JaneWB <brownjannick@att.net>
2018-09-18 20:30:27 -0500

    Add project files.

---
+++ StudentData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
