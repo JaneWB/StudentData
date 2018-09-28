5d307828194db2e1110a806f65066631fc39d387
JaneWB <brownjannick@att.net>
2018-09-28 12:53:44 -0500

    CG 11-1 Student Name

--- StudentData/Program.cs
+++ StudentData/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StudentData
 {
@@ -6,7 +9,32 @@ namespace StudentData
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StreamReader file = new StreamReader("C:\\Users\\brown\\source\\repos\\StudentData\\StudentData\\studentData.txt");
+
+            List<Student> students = new List<Student>();
+
+            string line;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] items = line.Split(" ");
+                Student s = new Student(items[0]);
+
+                for (int i = 1; i < items.Length; i++)
+                {
+                    s.AddScore(Int32.Parse(items[i]));
+                }
+
+                students.Add(s);
+                
+            }
+            foreach (Student item in students)
+            {
+                if (item.SixOrGreater())
+                Console.WriteLine(item.Name);
+              
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ StudentData/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudentData
+{
+    class Student
+    {
+        public List<int> scores = new List<int>();
+        public string Name { get; set; }
+
+
+        public Student(string name)
+        {
+            Name = name;
+        }
+
+        public void AddScore(int score)
+        {
+            scores.Add(score);
+        }
+
+        public bool SixOrGreater()
+        {
+
+            return scores.Count >= 7;
+            
+        }
+
+       
+
+
+    }   
+    
+
+}
